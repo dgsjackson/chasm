892df0f54e4c6093a538b188a18b2bfbd86708fb
Dylan Jackson <dylangsjackson@gmail.com>
2019-02-16 11:13:11 +0000

    feat(CSS): Added css bundling
    Installed CSS loaders and added them to webpack.config.
    Moved styles to css files.
    Added a body container.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-
-const styles = {
-    marginTop: 100,
-    marginLeft: 100,
-    border: '1px solid black'
-};
+import './App.css';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="app" style={styles}>
-                <h1> Welcome to my first React app! </h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <header id="app-header" className="navbar">
+          <section className="navbar-section">
+            <h3 id="header-brand" className="navbar-brand">CryptoMania!</h3>
+            <span className="navbar-item">Making the lucrative lucid</span>
+          </section>
+          <section>
+            <span>About</span>
+          </section>
+        </header>
+        <div id="content-container">
+          <div className="column col-sm-12 col-md-6 col-lg-4 col-3">
+            <input className="form-input" id="coin-list" type="text"></input>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default hot(module)(App);
