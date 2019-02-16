e7b1c8192335254f15545f95e62a165bf6c65439
Dylan Jackson <dylangsjackson@gmail.com>
2019-02-16 12:55:06 +0000

    feat(content): Added seletor and content-card prototypes

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -5,8 +5,25 @@ export default class Content extends React.Component {
   render() {
     return (
       <div id="content-container">
-        <div className="col-sm-12 col-md-6 col-lg-4 col-3">
-          <input className="form-input" id="coin-selector" type="text"></input>
+        <div id="selector-container">
+          <div className="col-sm-12 col-md-6 col-lg-4 col-3">
+            <input className="form-input" id="coin-selector" type="text"></input>
+          </div>
+          <ul className="menu col-sm-12 col-md-6 col-lg-4 col-3">
+            <li class="menu-item">
+              XRP
+            </li>
+            <li class="divider"></li>
+            <li class="menu-item">
+              BTC
+            </li>
+          </ul>
+        </div>
+        <div id="cards-container" className="col-sm-12 col-md-9 col-6">
+          <div class="card">
+            <h4 class="card-symbol">XRP</h4>
+            <div>0.33</div>
+          </div>
         </div>
       </div>
     );
